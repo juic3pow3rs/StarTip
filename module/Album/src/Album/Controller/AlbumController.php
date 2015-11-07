3bbe769c291d2c95a1e203032833802d291d8b24
juic3pow3rs <juic3pow3rs@users.noreply.github.com>
2015-11-07 15:59:36 +0100

    Album Example Modul (/public/index.php/album) fertig gestellt. Funktionen: - add - edit - delete

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -10,7 +10,8 @@ namespace Album\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
-use Zend\View\View;
+use Album\Model\Album;
+use Album\Form\AlbumForm;
 
 class AlbumController extends AbstractActionController {
     protected $albumTable;
@@ -23,12 +24,88 @@ class AlbumController extends AbstractActionController {
     }
 
     public function addAction() {
+        $form = new AlbumForm();
+        $form->get('submit')->setValue('Add');
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $album = new Album();
+            $form->setInputFilter($album->getInputFilter());
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $album->exchangeArray($form->getData());
+                $this->getAlbumTable()->saveAlbum($album);
+
+                //redirect to list of albums
+                return $this->redirect()->toRoute('album');
+            }
+        }
+        return array('form' => $form);
     }
 
     public function editAction() {
+        // Get the id from the route
+        // If the id is 0 redirect to addAction
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if (!$id) {
+            return $this->redirect()->toRoute('album', array(
+                'action' => 'add'
+            ));
+        }
+
+        try {
+            $album = $this->getAlbumTable()->getAlbum($id);
+        }
+        catch (\Exception $ex) {
+            return $this->redirect()->toRoute('album', array(
+                'action' => 'index'
+            ));
+        }
+
+        $form = new AlbumForm();
+        $form->bind($album);
+        $form->get('submit')->setAttribute('value', 'Edit');
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $form->setInputFilter($album->getInputFilter());
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $this->getAlbumTable()->saveAlbum($album);
+
+                return $this->redirect()->toRoute('album');
+            }
+        }
+        return array(
+            'id' => $id,
+            'form' => $form,
+        );
     }
 
     public function deleteAction() {
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if (!$id) {
+            return $this->redirect()->toRoute('album');
+        }
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $del = $request->getPost('del', 'No');
+
+            if ($del =='Yes') {
+                $id = (int) $request->getPost('id');
+                $this->getAlbumTable()->deleteAlbum($id);
+            }
+
+            return $this->redirect()->toRoute('album');
+        }
+
+        return array(
+            'id' => $id,
+            'album' => $this->getAlbumTable()->getAlbum($id),
+        );
     }
 
     public function getAlbumTable() {
--- module/Album/src/Album/Model/Album.php
+++ module/Album/src/Album/Model/Album.php
@@ -8,10 +8,15 @@
 
 namespace Album\Model;
 
-class Album {
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+
+class Album implements  InputFilterAwareInterface {
     public $id;
     public $artist;
     public $title;
+    protected $inputFilter;
 
     public function exchangeArray($data) {
         $this->id = (!empty($data['id'])) ? $data['id'] : null;
@@ -19,4 +24,82 @@ class Album {
         $this->title = (!empty($data['title'])) ? $data['title'] : null;
     }
 
+    public function getArrayCopy() {
+        return get_object_vars($this);
+    }
+
+    /**
+     * Set input filter
+     *
+     * @param  InputFilterInterface $inputFilter
+     * @return InputFilterAwareInterface
+     */
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new \Exception("Not used");
+    }
+
+    /**
+     * Retrieve input filter
+     *
+     * @return InputFilterInterface
+     */
+    public function getInputFilter()
+    {
+        if(!$this->inputFilter) {
+            $inputFilter = new InputFilter();
+            $inputFilter->add(
+                array(
+                    'name' => 'id',
+                    'required' => 'true',
+                    'filters' => array(
+                        array('name' => 'Int')
+                    ),
+                )
+            );
+            $inputFilter->add(
+                array(
+                    'name' => 'artist',
+                    'required' => 'true',
+                    'filters' => array(
+                        array('name' => 'StripTags'),
+                        array('name' => 'StringTrim'),
+                    ),
+                    'validators' => array(
+                        array(
+                            'name' => 'StringLength',
+                            'options' => array(
+                                'encoding' => 'UTF-8',
+                                'min' => 1,
+                                'max' => 100,
+                            ),
+                        ),
+                    ),
+                )
+            );
+            $inputFilter->add(
+                array(
+                    'name' => 'title',
+                    'required' => 'true',
+                    'filters' => array(
+                        array('name' => 'StripTags'),
+                        array('name' => 'StringTrim'),
+                    ),
+                    'validators' => array(
+                        array(
+                            'name' => 'StringLength',
+                            'options' => array(
+                                'encoding' => 'UTF-8',
+                                'min' => 1,
+                                'max' => 100,
+                            ),
+                        ),
+                    ),
+                )
+            );
+
+            $this->inputFilter = $inputFilter;
+        }
+        return $this->inputFilter;
+    }
 }
